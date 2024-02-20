5fb4c2075a65ed9f861c0e3b4dad8e288cb4b59c
tamvo0610 <tamvo0610@gmail.com>
2024-02-20 15:12:38 +0700

    Update Code

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,13 @@
 echo "===== Test Value: $1"
-inputs=$1
-echo "===== Test Inputs: ${inputs[path]}"
+# inputs=$1
+# echo "$inputs" | jq ".path"
+
+inputs_json=$(echo $1 | jq -r '.')
+path_value=$(echo "$inputs_json" | jq -r '.path')
+action_value=$(echo "$inputs_json" | jq -r '.action')
+cache_key_value=$(echo "$inputs_json" | jq -r '.cache-key')
+cache_dir_value=$(echo "$inputs_json" | jq -r '.cache-dir')
+echo "path=$path_value"
+echo "action=$action_value"
+echo "cache-key=$cache_key_value"
+echo "cache-dir=$cache_dir_value"
