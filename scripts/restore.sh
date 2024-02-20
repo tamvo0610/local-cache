ba09d34042d0944827b9c6577964d0b32b86323f
tamvo0610 <tamvo0610@gmail.com>
2024-02-20 17:56:02 +0700

    Update Code

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -3,8 +3,8 @@ if [ -d $CACHE_PATH ]; then
   mkdir -p $TARGET_PATH
   rsync -a $CACHE_PATH/ $TARGET_PATH
   echo "===== INFO: Cache restore success"
-  echo "cache_exist=true" >> "$GITHUB_OUTPUT"
+  echo "cache-hit=true" >> "$GITHUB_OUTPUT"
 else
   echo "===== INFO: Cache not exist, skip restore"
-  echo "cache_exist=false" >> "$GITHUB_OUTPUT"
+  echo "cache-hit=false" >> "$GITHUB_OUTPUT"
 fi
