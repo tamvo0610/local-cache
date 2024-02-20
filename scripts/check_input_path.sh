215fdc9eb943ebce73db7fb4452219fac7cc4c69
tamvo0610 <tamvo0610@gmail.com>
2024-02-20 16:24:37 +0700

    Update Code

---
+++ scripts/check_input_path.sh
@@ -0,0 +1,26 @@
+# value=$1
+
+# inputs=()
+# while IFS= read -r line; do
+#   inputs+=("$line")
+# done < $1
+# TARGET_PATH=${inputs[0]}
+# TARGET_ACTION=${inputs[1]}
+# CACHE_KEY=${inputs[2]}
+# CACHE_DIR=${inputs[3]}
+# CACHE_PATH=${inputs[4]}
+
+if [[ -z "${1}" ]]; then
+  echo "===== INFO: Path is required and not be empty."
+  exit 1
+fi
+export target_path=$1
+# if [[ -z "${CACHE_KEY}" ]]; then
+#   echo "===== INFO: Cache Key is required and not be empty."
+#   exit 1
+# fi
+
+# if [[ -z "${CACHE_DIR}" ]]; then
+#   echo "===== INFO: Cache Dir is required and not be empty."
+#   exit 1
+# fi
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,18 @@
-inputs_json=$(echo "$1" | jq -r '.')
-path_value=$(echo "$inputs_json" | jq -r '.path')
-action_value=$(echo "$inputs_json" | jq -r '.action')
-cache_key_value=$(echo "$inputs_json" | jq -r '.cache-key')
-cache_dir_value=$(echo "$inputs_json" | jq -r '.cache-dir')
+#!/bin/bash
+inputs=$1
+
+# Remove the curly braces {} and double quotes ""
+clean_inputs=$(echo "$inputs" | sed -e 's/[{}"]//g')
+
+# Convert commas , to whitespace
+clean_inputs=$(echo "$clean_inputs" | tr ',' '\n')
+
+# Use sed to extract the values
+path_value=$(echo "$clean_inputs" | grep 'path:' | sed 's/^.*: //')
+action_value=$(echo "$clean_inputs" | grep 'action:' | sed 's/^.*: //')
+cache_key_value=$(echo "$clean_inputs" | grep 'cache-key:' | sed 's/^.*: //')
+cache_dir_value=$(echo "$clean_inputs" | grep 'cache-dir:' | sed 's/^.*: //')
+
 echo "path=$path_value"
 echo "action=$action_value"
 echo "cache-key=$cache_key_value"
